01eb078a21922b343ceb7b42e67840cdf31b65e9
eshan <eshanhussain2004@gmail.com>
2026-01-10 20:33:15 -0500

    Created the following: - make_expression function can now work for division and subtraction operators
    - Variable class is now part of the Expr class as a subclass
    
    - Can now create expressions that involve variables, i.e., make_expressions("(a) + (0.5)")
    
    - Created "update_variable" for all expression instances. Allows for a preexisting variable to be updated to a new assignment.

--- Backend/Propositions.py
+++ Backend/Propositions.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 from typing import Union, Any
+from Sets import Set
+from sympy import Symbol
 
 for_all = ' ∀ '
 there_exists = ' ∃ '
@@ -13,7 +15,7 @@ Implies = ' ⇒ '
 if_and_only_if = ' ⇔ '
 connectives = [And, Not, Or, Implies, if_and_only_if]
 
-operators = ['+', '*']
+operators = ['+', '*', '-', '/']
 
 class Predicate:
     """
@@ -81,6 +83,39 @@ class Expr:
         """
         raise NotImplementedError
 
+    def update_variable(self, expression: str) -> bool:
+        """
+        Update the variable of this expression.
+
+        === Preconditions ===
+        expression is in the form of <existing_variable> = <new_value>
+        """
+
+        # Root could be a variable
+        if isinstance(self, Variable):
+            split = expression.split('=')
+
+            # Root could be another variable value
+            if self.name != split[0]:
+                return False
+            else:
+                if "." in split[1]:
+                    self.assignment = float(split[1])
+                else:
+                    self.assignment = int(split[1])
+                return True
+
+        # if this root is a binary operator
+        if isinstance(self, BinOp):
+            left = self.left.update_variable(expression)
+            right = self.right.update_variable(expression)
+
+            return True if left is True or right is True else False
+
+        # This expression is a number
+        else:
+            return False
+
 class Num(Expr):
     """An numeric constant literal.
 
@@ -117,7 +152,7 @@ class BinOp(Expr):
     right: the right operand
 
     === Representation Invariants ===
-    - self.op == '+' or self.op == '*'
+    - self.op == '+' or self.op == '*' or self.op == '-' or self.op == '\'
     """
     left: Expr
     op: str
@@ -146,14 +181,88 @@ class BinOp(Expr):
         """
         if self.op == '+':
             return self.left.evaluate() + self.right.evaluate()
+
+        if self.op == '-':
+            return self.left.evaluate() - self.right.evaluate()
+
+        if self.op == '/':
+            return self.left.evaluate() / self.right.evaluate()
+
         else:
             return self.left.evaluate() * self.right.evaluate()
 
     def __str__(self):
         return f"({self.left.__str__()}) {self.op} ({self.right.__str__()})"
 
+class Variable(Expr):
+    """
+    One of the primary mathematical objects used in proofs.
 
-def make_numerical_expression(expression:str) -> Expr:
+    ==Public Attributes==
+    name: The name/symbol of the variable.
+    domain: The domain of the variable, i.e., where the variable comes from
+    assignment: What object the variable is assigned to, i.e., int, function, set i.e.,
+    """
+    name: str
+    domain: Union[Set, None]
+    assignment: Any
+    Properties: list[Proposition]
+    _Sy_var: Symbol
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.domain = None
+        self.assignment = None
+        self._Sy_var = Symbol(name)
+        self.Properties = []
+
+    def assign(self, value: int) -> None:
+        """
+        Assigns the value to the variable.
+        """
+        self.assignment = value
+        # TODO check if assignment matches with domain, otherwise, raise an error
+
+    def define_domain(self, domain: Set) -> None:
+        self.domain = domain
+
+    def evaluate(self) -> Any:
+        """
+        Return the value of the variable
+        """
+        #TODO might want to change this for variables with conditions, i.e., for even numbers, x=2k
+
+        # Evaluate what the variable is
+        if self.assignment is not None:
+            return self.assignment
+
+        # Return the name of this variable
+        raise ValueError
+
+    def definition(self) -> str:
+        """
+        Return the definition of the variable.
+        """
+        # Return the domain and what variable it is equated too
+        if (self.assignment is not None) and (self.domain is not None):
+            return f"{self.name} ∈ {self.domain}, {self.name} = {self.assignment}"
+
+        # Return the assignment of the variable
+        if self.assignment is not None:
+            return f"{self.name} = {self.assignment}"
+
+        # Return only the domain
+        if self.domain is not None:
+            return f"{self.name} ∈ {self.domain}"
+
+        # TODO may want to raise an error here since the variable is undefined
+        else:
+            return f"Unknown variable: {self.name}"
+
+    def __str__(self):
+        return f"{self.assignment}" if self.assignment is not None else f"{self.name}"
+
+def make_expressions(expression:str) -> Expr:
     """
     Given a string, return an AST numerical object
     === Preconditions ==
@@ -162,24 +271,32 @@ def make_numerical_expression(expression:str) -> Expr:
 
     === EXAMPLES ===
     >>> e = "1"
-    >>> expr = make_numerical_expression(e)
+    >>> expr = make_expressions(e)
     >>> print(expr)
     1
 
     >>> e = "(4) + (-5)"
-    >>> expr = make_numerical_expression(e)
+    >>> expr = make_expressions(e)
     >>> print(expr)
     (-4) + (-5)
     """
     if '(' not in expression or ')' not in expression:
+
+        # Check if it is a float
         if '.' in expression:
             return Num(float(expression))
-        else:
+
+        # Try converting into an int
+        try:
             return Num(int(expression))
+
+        # Most likely a variable
+        except ValueError:
+            return Variable(expression)
+
     else:
         left_bracket_index, right_bracket_index = None, None
         left, right = None, None
-        middle_i = None
         scope = 0
         middle = None
 
@@ -202,9 +319,9 @@ def make_numerical_expression(expression:str) -> Expr:
                 left_bracket_index, right_bracket_index = None, None
 
                 if left is None:
-                    left = make_numerical_expression(sub_expression)
+                    left = make_expressions(sub_expression)
                 else:
-                    right = make_numerical_expression(sub_expression)
+                    right = make_expressions(sub_expression)
 
             if expression[i] in operators:
                 if left is not None and middle is None:
@@ -213,6 +330,23 @@ def make_numerical_expression(expression:str) -> Expr:
 
 
         return BinOp(left, middle, right)
+
+def make_variable(expression: str):
+    split = expression.split("=")
+
+    # variable is part of a "known" domain #TODO make this condition
+
+    # In the case where a user defined the variable to nothing
+    if len(split) != 2:
+        return Variable(split[0])
+
+    # If the variable is assigned to a value, assign
+    else:
+        var = Variable(split[0])
+        var.assign(int(split[1]))
+        return var
+
+
 
 
 
