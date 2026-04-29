66c754ec32846a299585d88aee65209add90741c
ETWBALL <etwcodes@gmail.com>
2026-04-29 14:03:32 -0400

    Updated the env schema so that, when using the env, we know what is expected for each variable, so that every team member in this project has access to the same types. Also updated the auth utility to create new access tokens, verify access tokens, and the same thing for refresh tokens. Each token has a protected header, issued at date and time, as well as the issuer, audience, expiration time (which varies between both tokens), and a signature from the server side.

--- Prove It/lib/AuthUtility/auth-utility.ts
+++ Prove It/lib/AuthUtility/auth-utility.ts
@@ -1,1 +1,53 @@
-import { fromJwt } from 'jose'
+import { SignJWT, jwtVerify } from "jose";
+import { env } from "@/lib/Validation/zodSchemas";
+import type { UserModel } from '@/app/generated/prisma/models/User'
+
+
+// ACCESS TOKEN GENERATION: Returns a new token with payload and expiry time
+export async function generateAccessToken(payload: { publicId: UserModel['publicId'] }){
+    return await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setIssuer(env.NEXT_PUBLIC_APP_URL)        
+    .setAudience(env.NEXT_PUBLIC_APP_URL)  
+    .setExpirationTime(env.ACCESS_TOKEN_EXPIRES_IN)
+    .sign(new TextEncoder().encode(env.ACCESS_TOKEN_JWT_SECRET));
+} 
+
+
+// ACCESS TOKEN VERIFICATION: If token valid, return payload. If invalid, return null
+export async function verifyAccessToken(token: string){
+    try {
+        return await jwtVerify(token, new TextEncoder().encode(env.ACCESS_TOKEN_JWT_SECRET), {
+            issuer: env.NEXT_PUBLIC_APP_URL,
+            audience: env.NEXT_PUBLIC_APP_URL,
+        });
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+// REFRESH TOKEN GENERATION: Returns a new token with payload and expiry time
+export async function generateRefreshToken(payload: { publicId: UserModel['publicId'] }){
+    return await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setIssuer(env.NEXT_PUBLIC_APP_URL)        
+    .setAudience(env.NEXT_PUBLIC_APP_URL)  
+    .setExpirationTime(env.REFRESH_TOKEN_EXPIRES_IN)
+    .sign(new TextEncoder().encode(env.REFRESH_TOKEN_JWT_SECRET));
+} 
+
+// REFRESH TOKEN VERIFICATION: If token valid, return payload. If invalid, return null
+export async function verifyRefreshToken(token: string){
+    try {
+        return await jwtVerify(token, new TextEncoder().encode(env.REFRESH_TOKEN_JWT_SECRET), {
+            issuer: env.NEXT_PUBLIC_APP_URL,
+            audience: env.NEXT_PUBLIC_APP_URL,
+        });
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
--- Prove It/lib/Validation/zodSchemas.ts
+++ Prove It/lib/Validation/zodSchemas.ts
@@ -30,8 +30,17 @@ export const LoginSchema = z.object({
 // Validation schema for environment variables
 export const envSchema = z.object({
     DATABASE_URL: z.string(),
-    JWT_SECRET: z.string().min(32, 'JWT_SECRET must be at least 32 characters'),
-    ACCESS_TOKEN_EXPIRES_IN: z.coerce.number().default(5),
-    REFRESH_TOKEN_EXPIRES_IN: z.coerce.number().default(60),
     BCRYPT_SALT_ROUNDS: z.coerce.number().default(10),
+    ACCESS_TOKEN_JWT_SECRET: z.string().min(32, 'ACCESS_TOKEN_JWT_SECRET must be at least 32 characters'),
+    ACCESS_TOKEN_EXPIRES_IN: z.string(),
+    REFRESH_TOKEN_JWT_SECRET: z.string().min(32, 'REFRESH_TOKEN_JWT_SECRET must be at least 32 characters'),
+    REFRESH_TOKEN_EXPIRES_IN: z.string(),
+    UPSTASH_REDIS_REST_URL: z.string(),
+    UPSTASH_REDIS_REST_TOKEN: z.string(),
+    NEXT_PUBLIC_APP_URL: z.string(),
+    NEXT_PUBLIC_TURNSTILE_SITE_KEY: z.string(),
+    TURNSTILE_SECRET_KEY: z.string(),
+    RESEND_API_KEY: z.string(),
   })
+
+export const env = envSchema.parse(process.env)
